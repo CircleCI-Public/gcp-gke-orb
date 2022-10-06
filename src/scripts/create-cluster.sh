698396c5bf18b4ab574fe3bf0433b0ba38cf4e92
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-10-06 10:07:01 -0700

    refactor: extract shell code in `create-cluster`

---
+++ src/scripts/create-cluster.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Import "utils.sh".
+eval "$SCRIPT_UTILS"
+expand_env_vars_with_prefix "ORB_PARAM_"
+
+create_command="create"
+
+# shellcheck disable=SC2034
+[ "$ORB_PARAM_ENABLE_AUTOPILOT" -eq 1 ] && create_command="create-auto"
+
+set -x
+gcloud container clusters "$create_command" "$ORB_PARAM_CLUSTER_NAME" "$ORB_PARAM_ADDITIONAL_ARGS"
+set +x
