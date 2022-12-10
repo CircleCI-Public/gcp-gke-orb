b0b791919405521e2f9814c2ef1aa975f7e6f66f
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-12-09 20:00:52 -0800

    refactor: extract scripts from delete-cluster

---
+++ src/scripts/delete-cluster.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+eval "$ORB_SCRIPT_UTILS"
+check_gcloud_status || exit $?
+expand_env_vars_with_prefix "ORB_EVAL_"
+
+set -x
+# shellcheck disable=SC2086
+gcloud container clusters delete "$ORB_EVAL_CLUSTER" --quiet $ORB_EVAL_ADDITIONAL_ARGS
+set +x
