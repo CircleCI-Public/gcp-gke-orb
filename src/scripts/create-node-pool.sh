75d155c2c4d9d26ef8a62025c418650f96d3343b
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-12-09 20:11:20 -0800

    refactor: remove noise from comment

--- src/scripts/create-node-pool.sh
+++ src/scripts/create-node-pool.sh
@@ -5,6 +5,6 @@ check_gcloud_status || exit $?
 expand_env_vars_with_prefix "ORB_EVAL_"
 
 set -x
-# shellcheck disable=SC2086 # We want to split the words.
+# shellcheck disable=SC2086
 gcloud container node-pools create "$ORB_EVAL_NODE_POOL" --cluster "$ORB_EVAL_CLUSTER" $ORB_EVAL_ADDITIONAL_ARGS
 set +x
